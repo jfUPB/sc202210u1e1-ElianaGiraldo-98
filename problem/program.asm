00cb6684a4da918c915b5058a36a7c4e1607f7ef
ElianaGiraldo-98 <eliana.giraldod@upb.edu.co>
2022-02-13 21:26:21 -0500

    Evalución

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,60 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
-(START)
-    @START
-    0;JMP
+// COLOCO AQUÍ TU NOMBRE COMPLETO Eliana Giraldo Duque
+// COLOCA AQUÍ TU ID 00321721
+// COLOCA AQUÍ TU CORREO ELECTRÓNICO eliana.giraldod@upb.edu.co
+(LOOP)
+(KEYF)
+@KBD 
+D=M
+@70 // "F"
+D=D-A
+@NEGRO
+D;JEQ
+@KEYF
+0;JMP
+   
+(BLACK)
+    @i 
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGE
+    @SCREEN 
+    D=A
+    @i 
+    A=D+M
+    M=-1 
+    @i 
+    M=M+1 // i++ 
+
+@NEGRO
+0;JMP
+(LOOP)
+(WHITE)
+
+(KEYC)
+@KBD
+D=M
+@67 // "C"
+D=D-A 
+@BLANCO
+D;JEQ
+@KEYC
+0;JMP
+
+(WHITELOOP)
+    @i 
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGE
+    @SCREEN
+    D=A
+    @i
+    A=D+M 
+    M=0 
+    @i 
+    M=M+1 
+@WHITELOOP
+0;JMP
